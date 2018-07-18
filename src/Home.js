6e77294f0cbe6173a1c5c9b026a64cd7b68a01f1
lidia-saf <lidiia.safiullina@yandex.ru>
2018-07-18 11:49:56 +0300

    updated css to have the gallery view for the pictures

--- src/Home.js
+++ src/Home.js
@@ -7,23 +7,21 @@ class Home extends Component {
     let picturesWrapper = [];
     let mappedPictures = this.props.pictures.map((pic) => {
       return(
-          <div key={pic.id} className="picture responsiveImage">
-            <img src={pic.pictures} alt="unopened moment of life" />
+          <div key={pic.id} className="cell">
+            <img src={pic.pictures} className="responsiveImage" alt="unopened moment of life" />
           </div>
       )
     });
-    picturesWrapper.push(<div className="grid" key="0">{mappedPictures.slice(0, 6)}</div>)
-    picturesWrapper.push(<div className="grid" key="1">{mappedPictures.slice(6, 11)}</div>)
-    picturesWrapper.push(<div className="grid" key="2">{mappedPictures.slice(11, 16)}</div>)
-    picturesWrapper.push(<div className="grid" key="3">{mappedPictures.slice(16, 21)}</div>)
-    picturesWrapper.push(<div className="grid" key="4">{mappedPictures.slice(21, 26)}</div>)
-    picturesWrapper.push(<div className="grid" key="5">{mappedPictures.slice(26, 31)}</div>)
+    picturesWrapper.push(<div className="grid" key="0">{mappedPictures.slice(0, 7)}</div>)
+    picturesWrapper.push(<div className="grid" key="1">{mappedPictures.slice(7, 13)}</div>)
+    picturesWrapper.push(<div className="grid" key="2">{mappedPictures.slice(13, 19)}</div>)
+    picturesWrapper.push(<div className="grid" key="3">{mappedPictures.slice(19, 25)}</div>)
+    picturesWrapper.push(<div className="grid" key="4">{mappedPictures.slice(25, 31)}</div>)
   return picturesWrapper
   }
   
   
   render() {
-    console.log(this.props.pictures)
     return (
       <div>
         <h2>Enjoy randomly selected pictures for moments of life:</h2>
@@ -37,7 +35,6 @@ class Home extends Component {
 }
 
 const mapStateToProps = state => {
-  console.log(state.pictures)
   return {
   pictures: state.pictures
 }
