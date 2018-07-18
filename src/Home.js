262095fba377611296d422a473c65c47c4499b72
lidia-saf <lidiia.safiullina@yandex.ru>
2018-07-18 11:31:51 +0300

    started to work on the design

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,25 @@ import "./home.css"
 import { connect } from 'react-redux'
 
 class Home extends Component {
+  gridCreator() {
+    let picturesWrapper = [];
+    let mappedPictures = this.props.pictures.map((pic) => {
+      return(
+          <div key={pic.id} className="picture responsiveImage">
+            <img src={pic.pictures} alt="unopened moment of life" />
+          </div>
+      )
+    });
+    picturesWrapper.push(<div className="grid" key="0">{mappedPictures.slice(0, 6)}</div>)
+    picturesWrapper.push(<div className="grid" key="1">{mappedPictures.slice(6, 11)}</div>)
+    picturesWrapper.push(<div className="grid" key="2">{mappedPictures.slice(11, 16)}</div>)
+    picturesWrapper.push(<div className="grid" key="3">{mappedPictures.slice(16, 21)}</div>)
+    picturesWrapper.push(<div className="grid" key="4">{mappedPictures.slice(21, 26)}</div>)
+    picturesWrapper.push(<div className="grid" key="5">{mappedPictures.slice(26, 31)}</div>)
+  return picturesWrapper
+  }
+  
+  
   render() {
     console.log(this.props.pictures)
     return (
@@ -10,16 +29,7 @@ class Home extends Component {
         <h2>Enjoy randomly selected pictures for moments of life:</h2>
         <br/>
         <div className="picturesContainer">
-         {this.props.pictures.map((pic) => {
-           return(
-             <div key={pic.id} className="onePicture">
-               <div key={pic.id} className="picture">
-                 <img src={pic.pictures} alt="unopened moment of life" />
-                 <div className="pictureDescription">{pic.description == null ? "Captured moment oflife" : pic.description}</div>
-               </div>
-             </div>
-           )
-         })}
+         {this.gridCreator()}
         </div>
       </div>
     );
